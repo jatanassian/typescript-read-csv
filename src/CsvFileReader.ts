44c2b2b2a1d43676b080165e1a2e40b3f5dae0b2
Julien Atanassian <julien.atanassian@gmail.com>
2023-05-11 21:06:00 -0400

    format data to date and numbers

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { dateStringToDate } from './utils';
 
 export class CsvFileReader {
 	data: string[][] = [];
@@ -11,6 +12,15 @@ export class CsvFileReader {
 				encoding: 'utf-8',
 			})
 			.split('\n')
-			.map((row: string): string[] => row.split(','));
+			.map((row: string): string[] => row.split(','))
+			.map((row: string[]): any => [
+				dateStringToDate(row[0]),
+				row[1],
+				row[2],
+				parseInt(row[3]),
+				parseInt(row[4]),
+				row[5],
+				row[6],
+			]);
 	}
 }
