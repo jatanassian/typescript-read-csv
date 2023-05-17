05c699f5afa4155932119853a55d4aacd1ddaa22
Julien Atanassian <julien.atanassian@gmail.com>
2023-05-17 16:37:43 -0400

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvReader = new CsvFileReader('football.csv');
+
+const matchReader = new MatchReader(csvReader);
+
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
 	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
 		manUnitedWins++;
 	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
