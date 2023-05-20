c45d9715d2a6f6f156e18ad1c3d18e54dd2fc6bf
Julien Atanassian <julien.atanassian@gmail.com>
2023-05-20 00:31:15 -0400

    create html report generator

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
 import { Summary } from './Summary';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 
 // Read csv file
@@ -11,9 +12,16 @@ const csvReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvReader);
 matchReader.load();
 
-// Analyze and report
-const summary = new Summary(
+// Analyze and console log report
+const logSummary = new Summary(
 	new WinsAnalysis('Man United'),
 	new ConsoleReport()
 );
-summary.buildAndPrintReport(matchReader.matches);
+logSummary.buildAndPrintReport(matchReader.matches);
+
+// Analyze and create html of report
+const htmlSummary = new Summary(
+	new WinsAnalysis('Man United'),
+	new HtmlReport('report')
+);
+htmlSummary.buildAndPrintReport(matchReader.matches);
