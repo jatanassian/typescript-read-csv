8558282e51d5067a16d2caef77a5fe5c5f26a951
Julien Atanassian <julien.atanassian@gmail.com>
2023-05-20 00:23:44 -0400

    implement summary in index file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
 import { CsvFileReader } from './CsvFileReader';
+import { Summary } from './Summary';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
 
+// Read csv file
 const csvReader = new CsvFileReader('football.csv');
 
+// Format data
 const matchReader = new MatchReader(csvReader);
-
 matchReader.load();
+
+// Analyze and report
+const summary = new Summary(
+	new WinsAnalysis('Man United'),
+	new ConsoleReport()
+);
+summary.buildAndPrintReport(matchReader.matches);
