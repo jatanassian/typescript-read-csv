5c65a2a3f3a5a213166a291e07461f9bbb297ea7
Julien Atanassian <julien.atanassian@gmail.com>
2023-05-19 23:57:01 -0400

    create wins analyzer

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,3 @@ const csvReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvReader);
 
 matchReader.load();
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-		manUnitedWins++;
-	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-		manUnitedWins++;
-	}
-}
-
-console.log('Man United wins:', manUnitedWins);
